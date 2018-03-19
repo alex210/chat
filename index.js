dcbbc52669c44c9af8325f7c2d36db859472fc60
Ваше Имя <you@example.com>
2018-03-19 15:59:51 +0000

    notification

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require("body-parser");
 
-app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res){
+//    res.sendFile(__dirname + '/index.html');
+// });
 
-app.get('/style', function(req, res){
-   res.sendFile(__dirname + '/style.css');
+// app.get('/style', function(req, res){
+//    res.sendFile(__dirname + '/style.css');
+// });
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+app.post('/event', urlencodedParser, function(req, res){
+   io.emit('event'+req.body.staff, req.body.message);
+   res.send();
 });
 
 io.on('connection', function(socket){
